13dba224e60db27ff83f09f8b8bacf8c6cf8fce0
joseignaciobauque <jirastorzabauque@frba.utn.edu.ar>
2026-05-14 18:56:07 -0300

    Agrega los Tests

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 - [✓] Aceptar el assignment y clonar el repositorio con el ejercicio
 - [✓] Ir a la carpeta donde descargaste el ejercicio.
 - [✓] Reemplazar nombre y legajo en el archivo README.md
-- [ ] Resolver el ejercicio siguiendo [un esquema de trabajo](https://github.com/pdep-utn/enunciados-miercoles-noche/blob/master/pages/haskell/trabajo.md), eso incluye
-- [ ] Ejecutar los tests con `stack test` y que den verde
-- [ ] A medida que vas resolviendo el ejercicio, subir [el progreso a git](https://github.com/pdep-utn/enunciados-miercoles-noche/blob/master/pages/git/resolverConflictos.md)
+- [✓] Resolver el ejercicio siguiendo [un esquema de trabajo](https://github.com/pdep-utn/enunciados-miercoles-noche/blob/master/pages/haskell/trabajo.md), eso incluye
+- [✓] Ejecutar los tests con `stack test` y que den verde
+- [✓] A medida que vas resolviendo el ejercicio, subir [el progreso a git](https://github.com/pdep-utn/enunciados-miercoles-noche/blob/master/pages/git/resolverConflictos.md)
 
 ## Nombre y legajo
 
--- src/Spec.hs
+++ src/Spec.hs
@@ -7,5 +7,39 @@ import Control.Exception (evaluate)
 correrTests :: IO ()
 correrTests = hspec $ do
     describe "TP 5" $ do
-        it "test de prueba" $ do
-            2 + 2 `shouldBe` 4
+        it "cuando se agranda una hamburguesa de carne se le agrega un pedazo de carne" $ do
+            agrandar (Hamburguesa 20 [Pan, Carne, Pan]) `shouldBe`  Hamburguesa 20 [Carne, Pan, Carne, Pan]
+        
+        it "cuando se agranda una hamburguesa de pollo se le agrega un pedazo de pollo" $ do
+          agrandar (Hamburguesa 20 [Pan, Pollo, Pan]) `shouldBe`  Hamburguesa 20 [Pollo, Pan, Pollo, Pan]
+
+        it "cuando se agranda una hamburguesa de pati vegano se le agrega un pedazo de pati vegano" $ do
+            agrandar (Hamburguesa 20 [Pan, PatiVegano, Pan]) `shouldBe`  Hamburguesa 20 [PatiVegano, Pan, PatiVegano, Pan]
+        
+        it "recibe un ingrediente y una hamburguesa y le agrega el ingrediente a la hamburguesa" $ do
+            agregarIngrediente Pollo cuartoDeLibra `shouldBe` cuartoDeLibra { precioBase = 20, ingredientes = [Pollo, Pan, Cheddar, Carne, Pan]}
+        
+        it "recibe un % de descuento, y devuelve la hamburguesa con ese descuento aplicado al precio base" $ do
+            descuento 10 cuartoDeLibra `shouldBe` cuartoDeLibra { precioBase = 18, ingredientes = [Pan, Cheddar, Carne, Pan]}
+        
+        it "la pdepBurger debe valer 110" $ do
+            precioHamburguesa pdepBurger `shouldBe` 110
+        
+        it "la dobleCuarto debe valer 84" $ do
+            precioHamburguesa dobleCuarto `shouldBe` 84
+        
+        it "la bigPdep debe valer 89" $ do
+            precioHamburguesa bigPdep `shouldBe` 89
+        
+        it "dada una hamburguesa, le agrega Papas y un descuento del 30%" $ do
+            delDia bigPdep `shouldBe` bigPdep { precioBase = 14, ingredientes = [Papas, Curry, Cheddar, Pan, Cheddar, Carne, Pan]}
+        
+        it "cambia todos los ingredientes base que hayan en la hamburguesa por PatiVegano" $ do
+           hacerVeggie cuartoDeLibra `shouldBe` cuartoDeLibra { precioBase = 20, ingredientes = [Pan, QuesoDeAlmendras, PatiVegano, Pan]}
+        
+        it "cambia el Pan que haya en la hamburguesa por PanIntegral" $ do
+            cambiarPanDePati cuartoDeLibra `shouldBe` cuartoDeLibra { precioBase = 20, ingredientes = [PanIntegral, Cheddar, Carne, PanIntegral]}
+        
+        
+
+
